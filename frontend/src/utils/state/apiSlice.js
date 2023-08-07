8273c10e5ccdc2ac70a8afd45c013a569bf909d5
Bora <b0rik556@gmail.com>
2023-08-07 16:47:03 +0300

    login using rtk query

--- frontend/src/utils/state/apiSlice.js
+++ frontend/src/utils/state/apiSlice.js
@@ -2,29 +2,48 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ 
+  baseQuery: fetchBaseQuery({
     baseUrl: 'http://localhost:9000',
-    credentials: 'include'
+    credentials: 'include',
   }),
-  endpoints: builder => ({
+  endpoints: (builder) => ({
     getPosts: builder.query({
-      query: () => '/posts'
+      query: () => '/posts',
     }),
     addNewPost: builder.mutation({
-      query: initialPost => ({
+      query: (initialPost) => ({
         url: '/posts/addPost',
         method: 'POST',
         body: initialPost,
-      })
+      }),
     }),
     addNewUser: builder.mutation({
       query: (userData) => ({
         url: '/users/create',
         method: 'POST',
         body: userData,
-      })
-    })
-  })
+      }),
+    }),
+    loginUser: builder.mutation({
+      query: (userData) => ({
+        url: '/users/login',
+        method: 'POST',
+        body: userData,
+      }),
+    }),
+    logoutUser: builder.mutation({
+      query: (userData) => ({
+        url: '/users/logout',
+        method: 'POST',
+      }),
+    }),
+  }),
 });
 
-export const { useGetPostsQuery, useAddNewPostMutation, useAddNewUserMutation } = apiSlice;
+export const {
+  useGetPostsQuery,
+  useAddNewPostMutation,
+  useAddNewUserMutation,
+  useLoginUserMutation,
+  useLogoutUserMutation,
+} = apiSlice;
