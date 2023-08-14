11c50daa500234c91afa070350b6265fb0719a3f
Bora <b0rik556@gmail.com>
2023-08-14 18:07:20 +0300

    added friends list avatar placeholder

--- frontend/src/components/friends/friend.jsx
+++ frontend/src/components/friends/friend.jsx
@@ -2,13 +2,15 @@ import { Link } from 'react-router-dom';
 
 import '../../styles/friends/friend.css';
 
+import avatarPlaceHolder from '../../assets/avatar-placeholder.png';
+
 const Friend = ({ user }) => {
   const { _id, name, image } = user;
 
   return (
     <div className="friend">
       <div className="friend__image">
-        <img src={image} alt={name} />
+        <img src={image ? image : avatarPlaceHolder} alt={name} />
       </div>
       <Link to={''} className="friend__name">{name}</Link>
     </div>
