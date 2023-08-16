ca177563b44723cf09b0408d725754edf774a3ef
Bora <b0rik556@gmail.com>
2023-08-16 10:03:05 +0300

    added comment delete button

--- frontend/src/components/post/comment.jsx
+++ frontend/src/components/post/comment.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import '../../styles/post/comment.css';
 
 import avatarPlaceHolder from '../../assets/avatar-placeholder.png';
 
 const Comment = ({ comment }) => {
+  const { _id: currentUserId } = useSelector(state => state.user.data);
   const { author, content, date } = comment;
-
+  const isMyComment = currentUserId === author._id;
   return (
     <div className="comment">
       <div className="comment__header">
@@ -17,6 +19,7 @@ const Comment = ({ comment }) => {
           />
         </div>
         <Link to={`/users/${author._id}`} className="comment__name">{author.name}</Link>
+        {isMyComment && <button className="comment__delete">X</button>}
       </div>
       <div className="comment__content">{content}</div>
       <div className="comment__date">{new Date(date).toDateString()}</div>
